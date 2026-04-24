55f2bbb4c13312f48bd8d2bf71d02ab4e61d41a1
igorJPG <igordias2009@gmail.com>
2026-04-24 20:58:31 -0300

    //

---
+++ src/Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Quantos termos deseja exibir? ");
+        int n = scanner.nextInt();
+
+        int a = 0, b = 1;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+
+    }
+}
